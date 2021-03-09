1ba76bb50962b450337de6d67e4d42d4cbd71162
maywoollcott <maywoollcott@gmail.com>
2021-03-08 21:54:38 -0500

    styles stats page

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -13,15 +13,55 @@ const Stats = () => {
 
 
   return (
-    <div className="statscard">
-      <h1>STATS</h1>
-      <h2>Reading sessions: {totalReadingSessions}</h2>
-      <h2>Minutes read: {totalReadingTime}</h2>
-      <h2>Pages read: {totalPagesRead}</h2>
-      <h2>Books finished: {totalBooksFinished}</h2>
-      <h2>Average reading pace: {Math.round(totalPagesRead/totalReadingTime*60)} pages per hour</h2>
-      <h2>Longest reading session: {Math.max(...value.sessions.map(session => session.minutes), 0)} minutes</h2>
-      <h2>Streak: {value.streak}</h2>
+    <div>
+      <div className="statscontainer">
+        <div className="stats">
+          <div className="circle">
+            <h2>{totalReadingSessions}</h2>
+          </div>
+          <h3>READING SESSIONS</h3>
+        </div>
+        <div className="stats">
+          <div className="circle">
+            <h2>{totalReadingTime}</h2>
+          </div>
+          <h3>MINUTES READ</h3>
+        </div>
+        <div className="stats">
+          <div className="circle">
+            <h2>{totalPagesRead}</h2>
+          </div>
+          <h3>PAGES READ</h3>
+        </div>
+        {/* <div className="stats">
+          <div className="circle">
+            <h2>{totalBooksFinished}</h2>
+          </div>
+          <h3>BOOKS FINISHED</h3>
+        </div> */}
+        <div className="stats">
+          <div className="circle">
+            <h2>{Math.round(totalPagesRead/totalReadingTime*60)}</h2>
+          </div>
+          <h3>AVERAGE READING PACE</h3>
+          <p>(pages read per hour)</p>
+        </div>
+        <div className="stats">
+          <div className="circle">
+            <h2>{value.streak}</h2>
+          </div>
+          <h3>CURRENT STREAK</h3>
+          <p>(in days)</p>
+        </div>
+        <div className="stats">
+          <div className="circle">
+            <h2>{Math.max(...value.sessions.map(session => session.minutes), 0)}</h2>
+          </div>
+          <h3>LONGEST READING SESSION</h3>
+          <p>(in minutes)</p>
+        </div>
+      </div>
+      
     </div>
   )
 }
